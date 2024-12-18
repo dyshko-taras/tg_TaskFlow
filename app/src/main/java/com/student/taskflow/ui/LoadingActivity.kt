64040c0cd9739f5add87680652290b1767f4bd8b
taras_dyshko <dyshkotaras@gmail.com>
2024-12-18 19:46:16 +0200

    First commit

---
+++ app/src/main/java/com/student/taskflow/ui/LoadingActivity.kt
@@ -0,0 +1,35 @@
+package com.student.taskflow.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.student.taskflow.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class LoadingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_loading)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        navigateToMain()
+    }
+
+    private fun navigateToMain() {
+        lifecycleScope.launch() {
+            delay(2000) // Delay for 2 seconds
+            val intent = Intent(this@LoadingActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
