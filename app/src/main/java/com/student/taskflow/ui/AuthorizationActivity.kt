0f29b3d71922af79cf317ac8af1e51eb4c36c278
taras_dyshko <dyshkotaras@gmail.com>
2024-12-23 21:26:15 +0200

    Add AuthorizationActivity xml

---
+++ app/src/main/java/com/student/taskflow/ui/AuthorizationActivity.kt
@@ -0,0 +1,16 @@
+package com.student.taskflow.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.student.taskflow.R
+
+class AuthorizationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_authorization)
+    }
+}
--- app/src/main/java/com/student/taskflow/ui/RegistrationActivity.kt
+++ app/src/main/java/com/student/taskflow/ui/RegistrationActivity.kt
@@ -1,35 +1,21 @@
 package com.student.taskflow.ui
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.lifecycleScope
 import com.student.taskflow.R
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
-class LoadingActivity : AppCompatActivity() {
+class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_loading)
+        setContentView(R.layout.activity_registration)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        navigateToAuthorization()
-    }
-
-    private fun navigateToAuthorization() {
-        lifecycleScope.launch() {
-//            delay(2000) // Delay for 2 seconds
-            val intent = Intent(this@LoadingActivity, AuthorizationActivity::class.java)
-            startActivity(intent)
-            finish()
         }
     }
 }
