219f6a418d4ce8d8a3e652a2695ecb9722e08906
taras_dyshko <dyshkotaras@gmail.com>
2024-12-27 03:06:06 +0200

    Add AddTaskActivity xml

---
+++ app/src/main/java/com/student/taskflow/ui/AddTaskActivity.kt
@@ -0,0 +1,21 @@
+package com.student.taskflow.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.student.taskflow.R
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_add_task)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(v.paddingLeft, systemBars.top, v.paddingRight, systemBars.bottom)
+            insets
+        }
+    }
+}
